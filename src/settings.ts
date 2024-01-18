aaedace4894f6724453360b3e6611bde62232b83
a_dovhaliuk <a.dovhaliuk@svit.io>
2024-01-18 18:15:09 +0200

    edited name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'ExampleHomebridgePlatform';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'notcurrentuser-home-bridge-test-plugin';
+export const PLUGIN_NAME = 'notcurrentuser-home-bridge-test-plugin2';
